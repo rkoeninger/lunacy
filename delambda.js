32035de5d072d487634a009c835cfd290f3bb447
Robert Koeninger <rkoeninger@att.net>
2018-11-14 14:42:40 -0500

    Cleaned up impl, only does property access for now

--- delambda.js
+++ delambda.js
@@ -1,33 +1,25 @@
-function getBound(object, property) {
-  const value = object[property];
-  return typeof value === "function" ? value.bind(object) : value;
-}
+{
+  const getBound = (object, property) => {
+    const value = object[property];
+    return typeof value === "function" ? value.bind(object) : value;
+  };
 
-const root = {};
-
-function extend(select) {
-  return new Proxy(select, {
-    get(f, property, _2) {
-      return extend(arg => getBound(f(arg), property));
-    },
-    apply(f, _1, args) {
-      // TODO: if any args are the root delambda proxy, make new
-      //       selector that partially applies inner function
-      const wildcardIndices = args
-        .map((x, i) => x === root ? i : undefined)
-        .filter(x => x !== undefined);
-      if (wildcardIndices.length > 0) {
-        // TODO: take arg(s) here and put in place of wildcard
-        //       values in args from outer scope
-        console.log("partial application time: " + wildcardIndices);
-        return extend(arg => "composed proxy goes here");
-      } else {
+  const extend = select => {
+    return new Proxy(select, {
+      get(f, property, _2) {
+        return extend(arg => getBound(f(arg), property));
+      },
+      apply(f, _1, args) {
         return f(...args);
       }
-    }
-  });
-}
+    });
+  };
 
-if (module) {
-  module.exports = extend(x => x);
+  const root = extend(x => x);
+
+  if (typeof module !== "undefined") {
+    module.exports = root;
+  } else {
+    window.delambda = root;
+  }
 }
--- spec/delambda_spec.js
+++ spec/delambda_spec.js
@@ -1,19 +1,19 @@
-const $ = require("../delambda");
+const _ = require("../delambda");
 
 describe("delambda", () => {
   it("single property accessor", () => {
     const p = x => ({ p: x });
     const xs = [1, 2, 3];
-    expect(xs.map(p).map($.p)).toEqual(xs);
+    expect(xs.map(p).map(_.p)).toEqual(xs);
   });
   it("nested property accessor", () => {
     const pq = x => ({ p: { q: x } });
     const xs = [1, 2, 3];
-    expect(xs.map(pq).map($.p.q)).toEqual(xs);
+    expect(xs.map(pq).map(_.p.q)).toEqual(xs);
   });
   it("double-nested property accessor", () => {
     const pqr = x => ({ p: { q: { r: x } } });
     const xs = [1, 2, 3];
-    expect(xs.map(pqr).map($.p.q.r)).toEqual(xs);
+    expect(xs.map(pqr).map(_.p.q.r)).toEqual(xs);
   });
 });
